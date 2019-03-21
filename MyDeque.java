5b991ad13f98164f2d09706c188de10dd7ff1419
Jackie Lin <jlin00@stuy.edu>
2019-03-21 09:40:08 -0400

    added dummy methods

---
+++ MyDeque.java
@@ -0,0 +1,45 @@
+public class MyDeque<E>{
+  private E[] data;
+  private int size, start, end;
+
+  public MyDeque(){
+
+  }
+
+  public MyDeque(int initialCapacity){
+
+  }
+
+  public int size(){
+    return 0;
+  }
+
+  public String toString(){
+    return "";
+  }
+
+  public void addFirst(E element){
+
+  }
+
+  public void addLast(E element){
+
+  }
+
+  public E removeFirst(E element){
+    return element;
+  }
+
+  public E removeLast(E element){
+    return element;
+  }
+
+  public E getFirst(E element){
+    return element;
+  }
+
+  public E getLast(E element){
+    return element;
+  }
+
+}
