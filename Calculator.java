536253146793796a782a062e7260bf0da82fdf8b
Jackie Lin <jlin00@stuy.edu>
2019-03-25 10:13:49 -0400

    added code to convert string into operators and numbers

--- Calculator.java
+++ Calculator.java
@@ -1,8 +1,40 @@
+import java.util.*;
+
 public class Calculator{
     /*Evaluate a postfix expression stored in s.
      *Assume valid postfix notation, separated by spaces.
      */
     public static double eval(String s){
+      MyDeque<Double> stack = new MyDeque<Double>();
+
+      String[] split = s.split(" "); //split the parameter
+      System.out.println(Arrays.toString(split)); //for debugging purposes
+
+      for (int i = 0; i < split.length; i++){
+        //System.out.println(split[i]); //for debugging purposes;
+        if (split[i].equals("+")){
+          System.out.println("plus");
+        }
+        if (split[i].equals("-")){
+          System.out.println("minus");
+        }
+        if (split[i].equals("*")){
+          System.out.println("times");
+        }
+        if (split[i].equals("/")){
+          System.out.println("divide");
+        }
+        if (split[i].equals("%")){
+          System.out.println("remainder");
+        }
+        else{
+          System.out.println("number");
+        }
+      }
       return 0.0;
+    }
+
+    public static void main(String[] args) {
+      System.out.println(eval("9 9 +"));
     }
 }
