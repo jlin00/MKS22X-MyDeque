e06bc4011ce7e9ce05d21d71a51e86924649645f
Jackie Lin <jlin00@stuy.edu>
2019-03-25 13:52:02 -0400

    added code to adjust for commutative property

--- Calculator.java
+++ Calculator.java
@@ -8,40 +8,52 @@ public class Calculator{
       MyDeque<Double> stack = new MyDeque<Double>();
 
       String[] split = s.split(" "); //split the parameter
-      System.out.println(Arrays.toString(split)); //for debugging purposes
+      //System.out.println(Arrays.toString(split)); //for debugging purposes
 
-      for (int i = 0; i < split.length; i++){
+      for (String value: split){
         //System.out.println(value); //for debugging purposes;
-        String value = split[i];
         if (value.equals("+")){
           stack.addLast(stack.removeLast() + stack.removeLast());
-          System.out.println("plus");
+          //System.out.println("plus");
         }
         else if (value.equals("-")){
-          stack.addLast(stack.removeLast() - stack.removeLast());
-          System.out.println("minus");
+          double second = stack.removeLast();
+          double first = stack.removeLast();
+          stack.addLast(first - second);
+          //System.out.println("minus");
         }
         else if (value.equals("*")){
           stack.addLast(stack.removeLast() * stack.removeLast());
-          System.out.println("times");
+          //System.out.println("times");
         }
         else if (value.equals("/")){
-          stack.addLast(stack.removeLast() / stack.removeLast());
-          System.out.println("divide");
+          double second = stack.removeLast();
+          double first = stack.removeLast();
+          stack.addLast(first / second);
+          //System.out.println("divide");
         }
         else if (value.equals("%")){
-          stack.addLast(stack.removeLast() % stack.removeLast());
-          System.out.println("remainder");
+          double second = stack.removeLast();
+          double first = stack.removeLast();
+          stack.addLast(first % second);
+          //System.out.println("remainder");
         }
         else{
           stack.addLast(Double.parseDouble(value));
-          System.out.println("number");
+          //System.out.println("number");
         }
       }
-      return stack.getLast();
+      return stack.getFirst();
     }
 
+
     public static void main(String[] args) {
-      System.out.println(eval("9 9 +"));
+      System.out.println(eval("9 9 +")); //18.0
+      System.out.println(eval("10 2.0 +")); //12.0
+      System.out.println(eval("11 3 - 4 + 2.5 *")); //30.0
+      System.out.println(eval("8 2 + 99 9 - * 2 + 9 -")); //893.0
+      System.out.println(eval("1 2 3 4 5 + * - -")); //26.0
+      System.out.println(eval("9 8 -")); //1.0
+
     }
 }
